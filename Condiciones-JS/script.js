5362e73c9b9736b718219a40a3939e9763bba2bd
ivaniraldi <iraldiban@gmail.com>
2025-06-24 22:16:56 -0300

    Condiciones JS

---
+++ Condiciones-JS/script.js
@@ -0,0 +1,17 @@
+let num1 = 2
+
+let num2 = "2"
+
+
+//  >  <  mayor y menor
+//  >= <= mayor e igual y menor e igual
+// == igualdad
+
+//Comparadores estrictos (comparan valor y tipo de dato)
+// === igualdad estricta (valor como tipo de dato)
+// >== 
+// <==  
+
+if(num1 === num2){
+    console.log("Los valores son iguales")
+}
